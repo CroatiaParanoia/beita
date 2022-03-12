6c6bcbf88103af5581061d6e72ba783add116af5
croatialu <wuli.croatia@foxmail.com>
2022-03-12 14:40:07 +0800

    refactor: 区分环境

--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,10 +1,28 @@
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { PageContainerProps } from './types';
 import { useTitle } from 'ahooks';
 import clsx from 'clsx';
 
 export const PageContainer: FC<PageContainerProps> = ({ children, title, className }) => {
-  useTitle(title);
+  const realTitle = useMemo(() => {
+    if (import.meta.env.VITE_API_ENV === 'alpha') {
+      return `【测试环境】${title}`;
+    }
+
+    return title;
+  }, [title]);
+
+  useTitle(realTitle);
 
   return <div className={clsx('page', className)}>{children}</div>;
 };
+
+const a = 1;
+
+const fn = () => {
+  const a = 2;
+
+  console.log(a);
+};
+
+fn();
